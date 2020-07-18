a08261797dbf5e63937f55a90d99dfbbb8f41a4f
Olamide Ogunde <oogunde0413@gmail.com>
2020-07-17 17:41:58 -0700

    added tests

--- Code Challenges/401 Code Challenges/StacksAndQueues/XUnitTestProject1/UnitTest1.cs
+++ Code Challenges/401 Code Challenges/StacksAndQueues/XUnitTestProject1/UnitTest1.cs
@@ -23,17 +23,60 @@ namespace XUnitTestProject1
 
 
         [Fact]
-        public void TestPop()
+        public void CanPushMultipleNodesOnTheStack()
         {
             //Arrange
+
             Stack stack = new Stack();
+            string expected = "peanut";
 
             //Act
-            stack.Pop();
+            stack.Push("candy cane");
+            stack.Push("peanut");
 
             //Assert
-            Assert.Equal("candy cane", stack.Temp.Value);
+            string returnFromMethod = $"{stack.Peek()}";
 
+            Assert.Equal(expected, returnFromMethod);
+            Assert.NotNull(stack.Peek());
+        }
+
+        [Fact]
+        public void CanPopNodeOffStack()
+        {
+            Stack stack = new Stack();
+            string expected = "peanut";
+
+            stack.Push("candy cane");
+            stack.Push("peanut");
+            Node returnFromMethod = stack.Pop();
+            Assert.Equal(expected, returnFromMethod.Value);
+        }
+
+
+        [Fact]
+        public void CanEmptyStackByPoppin()
+        {
+            Stack stack = new Stack();
+            stack.Push("candy cane");
+            stack.Push("peanut");
+            stack.Pop();
+            stack.Pop();
+
+            Exception e = Assert.Throws<System.Exception>(() => stack.Peek());
+            string error = "Empty stack";
+
+            Assert.Equal(error, e.Message);
+        }
+        [Fact]
+        public void PeekReturnTopValue()
+        {
+            Stack stack = new Stack();
+
+            stack.Push("candy cane");
+            stack.Push("peanut");
+            string returnFromMethod = stack.Peek();
+            Assert.Equal("peanut", returnFromMethod);
         }
     }
 }
